20c4e949dbd097f0518445bc16e5ef7f1213bce3
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-11 14:28:20 +1000

    innerHTML example, which shouldn't be continue to use after HTML5 comes out

--- Code/myscript13.js
+++ Code/myscript13.js
@@ -7,6 +7,24 @@ window.onload = function() {
 
 }
 
+/*
 function runTheExample() {
     alert('running the example');
+}
+*/
+
+function runTheExample() {
+
+    var myElement = document.getElementById('second');
+
+    var myNodeName = myElement.nodeName;
+    // alert(myNodeName); // This is to get the HTML file DOM Object Name, which is "p" object here for ID = second
+
+    if (myElement != null) {
+        alert (myElement.innerHTML); //display HTML content inside <p> object for ID = second;
+        // It is 'dangerous' to continue to use 'innerHTML' after HTML5 comes out.
+        // People would rely on JQuery to replace 'innerHTML', due to various browser support.
+    };
+
+
 }
