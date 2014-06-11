c79a1a2c5965ad47c9fafd2f599575d5927ca328
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-11 16:29:14 +1000

    usage of list in JS

--- Code/myscript13.js
+++ Code/myscript13.js
@@ -25,8 +25,19 @@ function runTheExample() {
         // It is 'dangerous' to continue to use 'innerHTML' after HTML5 comes out.
         // People would rely on JQuery to replace 'innerHTML', due to various browser support.
     };
-*/
-// innerHTML can also be used to change the content of HTML file:
+
+// innerHTML can also be used to change the content of HTML file when using the statements below:
     document.getElementById('second').innerHTML = "See how I set the text here?"
+    // However, it is not an permanent change -- the HTML file itself did not save & store this change;
+    // Only when THIS JS file is used and this statement is enabled, then this change is real/displayed.
+*/
+
+var listOfParagraphs = document.getElementsByTagName('p');
+
+//alert(listOfParagraphs.length); //display how many Paragraphs the HTML file has
+
+var secondParagraph = listOfParagraphs[1];
+
+alert(secondParagraph.innerHTML);
 
 }
