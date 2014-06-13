0db89090a08ed9e92d4af103a4ca583bd77830b8
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-13 11:39:32 +1000

    starting JQuery

---
+++ Code/myscript14.js
@@ -0,0 +1,17 @@
+/*
+document is the entire DOM
+ready: when DOM is loaded and ready
+function(): handler function: when DOM ready, exec the function()
+which is alert "this works!"
+
+jQuery(document).ready(function() {
+    // start up code goes here
+    alert("this works!");
+})
+*/
+
+// use '$' sign as shortcut of 'jQuery'
+$(document).ready(function() {
+    // start up code goes here
+    alert("this works!");
+})
