cd626180586ebb1a7f6a18325636ebcb492d5dda
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-16 23:56:09 +1000

    Verify HTML5 & JS definition matching, before doing any developments. It would save confusing & debug / troubleshooting time in the long run

---
+++ Code/myscript16.js
@@ -0,0 +1,8 @@
+// Before doing anything else on JS development, try this alert() first to make sure
+// base definition between HTML5 and JS file is fine; that will save a lot of debugging
+// time in the long run. Also you have the chance to check HTML5 & CSS3 matching
+$(function() {
+    $('#bobhead').click(function() {
+        alert('verified that HTML and JS file works fine, before doing anything else');
+    })
+});
