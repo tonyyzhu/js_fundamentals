97eaeff439955e7248927d0454ea946c9dc71fbb
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-18 00:13:12 +1000

    Just by check & copy JQuery UI Documentation, I copy/implemented this datepicker function, by copy/add this demo sample statement I need into the HTML and JS files

--- Code/myscript17.js
+++ Code/myscript17.js
@@ -5,4 +5,6 @@ $(function() {
     $("#tabs").tabs();
     // Below "Add" Method no longer available in JQuery 1.10.3. Was available in version 1.8.17:
     //$("#tabs").tabs().tabs("add","myC9JS16.html","Click-a-Bob");
+    $("#datepicker").datepicker();
+    // But, I still checked JQuery UI page and copy/implemented this datepicker function! Cool..
 });
