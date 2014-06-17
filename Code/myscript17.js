3a861caffdce082cc90aa5382c54a9510a3ef819
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-18 00:05:14 +1000

    JQuery UI (Dymanic) Add Method no longer available in latest version 1.10.3 of 2014. Was available in 1.8.17 of 2012

--- Code/myscript17.js
+++ Code/myscript17.js
@@ -3,4 +3,6 @@ $(function() {
     // Test js before development
     // alert('got here');  == Works fine, so comment off
     $("#tabs").tabs();
+    // Below "Add" Method no longer available in JQuery 1.10.3. Was available in version 1.8.17:
+    //$("#tabs").tabs().tabs("add","myC9JS16.html","Click-a-Bob");
 });
