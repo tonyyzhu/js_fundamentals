59005495496f97521a8ca410a9b5247eb034b0c0
tonyyzhu <tonyyzhu@tpg.com.au>
2014-06-19 16:05:58 +1000

    can't test error alert function due to all files on HDD not a web server

--- Code/myscript19.js
+++ Code/myscript19.js
@@ -15,7 +15,7 @@ $(function() {
             });
      },
     statusCode: {
-        404: function() {
+        404: function() {   //This one won't work, because 404 error message only comes from a Web Server, and currently all files are on HDD instead.
             alert('There was a problem with the server. Try again soon!');
         }
       }
